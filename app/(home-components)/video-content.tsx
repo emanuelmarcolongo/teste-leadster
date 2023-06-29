a431c85afb7405f33f8a8cc8a2eafe2ae73b76f2
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-06-29 20:45:28 -0300

    chore: change modal to interact onwy ith VideoContainer

--- app/(home-components)/video-content.tsx
+++ app/(home-components)/video-content.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useEffect } from "react";
 import Image from "next/image";
 import { useState } from "react";
@@ -9,57 +9,63 @@ interface VideoModalProps {
 }
 
 export default function VideoContent() {
-    const [showModal, setShowModal] = useState(false);
-    
-    useEffect(() => {
-        const handleEsc = (event: KeyboardEvent) => {
-           if (event.key === "Escape") {
-            setShowModal(false);
-          }
-        };
-        window.addEventListener('keydown', handleEsc);
-    
-        return () => {
-          window.removeEventListener('keydown', handleEsc);
-        };
-      }, []);
-
   return (
     <main className="xl:w-[60%] mx-auto">
       <Filters></Filters>
-      <VideosContainer setShowModal={setShowModal}></VideosContainer>
-      <VideoModal showModal={showModal} setShowModal={setShowModal}/>
+      <VideosContainer></VideosContainer>
     </main>
   );
 }
 
-function VideosContainer({setShowModal}: VideoModalProps) {
+function VideosContainer() {
   const maxVideosOnDisplay = Array.from({ length: 9 }, (_, index) => index);
 
   return (
     <div className="flex justify-center w-full">
       <div className="grid lg:grid-cols-3 gap-10">
         {maxVideosOnDisplay.map((i) => (
-          <VideoContainer setShowModal={setShowModal}/>
+          <VideoContainer />
         ))}
       </div>
     </div>
   );
 }
 
-function VideoContainer({setShowModal}: VideoModalProps) {
+function VideoContainer() {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setShowModal(false);
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
   return (
-    <div onClick={()=> setShowModal(true)} className=" flex flex-col items-center justify-center bg-white rounded-xl drop-shadow-2xl shadow-black cursor-pointer">
-      <Image
-        height={250}
-        width={1000}
-        alt="Video Thumbnail"
-        src={"/imgs/thumbnail.png"}
-      ></Image>
-      <p className="text-fontColor text-md font-semibold  px-4 py-2">
-        Como aumentar sua geração de Leads feat. Traktor
-      </p>
-    </div>
+    <>
+      <div
+        onClick={() => setShowModal(true)}
+        className=" flex flex-col items-center justify-center bg-white rounded-xl drop-shadow-2xl shadow-black cursor-pointer"
+      >
+        <Image
+          height={250}
+          width={1000}
+          alt="Video Thumbnail"
+          src={"/imgs/thumbnail.png"}
+        ></Image>
+        <p className="text-fontColor text-md font-semibold  px-4 py-2">
+          Como aumentar sua geração de Leads feat. Traktor
+        </p>
+       
+      </div>
+      <VideoModal showModal={showModal} setShowModal={setShowModal} />
+    </>
   );
 }
 
@@ -97,29 +103,40 @@ function Filters() {
   );
 }
 
-function VideoModal({showModal, setShowModal}: VideoModalProps) {
-    
-
+function VideoModal({ showModal, setShowModal }: VideoModalProps) {
   return (
-    <div onClick={() => setShowModal(false)} className={` w-[100%] h-[100vh] bg-main bg-opacity-20 fixed bottom-0 left-0 flex items-center justify-center ${showModal ? "block" : "hidden"}`}>
-        
-      <div onClick={(e)=> e.stopPropagation()} className="bg-white w-[450px] rounded-xl mx-auto my-auto relative">
-      <button onClick={()=> setShowModal(false)} className="absolute top-3 right-3 w-[40px] h-[40px]">X</button>
+    <div
+      onClick={() => setShowModal(false)}
+      className={` w-[100%] h-[100vh] bg-main bg-opacity-20 fixed bottom-0 left-0 flex items-center justify-center z-10 ${
+        showModal ? "block" : "hidden"
+      }`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white w-[450px] rounded-xl mx-auto my-auto relative"
+      >
+        <button
+          onClick={() => setShowModal(false)}
+          className="absolute top-3 right-3 w-[40px] h-[40px]"
+        >
+          X
+        </button>
         <p className="text-fontColor font-semibold self-center mx-auto  p-5 w-[300px]">
           <span className="text-main">Webinar:</span> Como aumentar sua Geração
           de Leads feat. Traktor
         </p>
 
-        {showModal &&  <iframe
-          width="450"
-          height="300"
-          src="https://www.youtube.com/embed/a89Htfx0YKo"
-        />
-}
-       
+        {showModal && (
+          <iframe
+            width="450"
+            height="300"
+            src="https://www.youtube.com/embed/a89Htfx0YKo"
+          />
+        )}
+
         <div className="py-2 px-5">
           <p className="text-fontColor font-semibold">Descrição:</p>
-          <hr className="mx-auto my-6 h-[1px] w-full " ></hr>
+          <hr className="mx-auto my-6 h-[1px] w-full "></hr>
           <p className="text-sm text-fontColor">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad
             rem distinctio eum aliquam, tenetur magni incidunt explicabo!
