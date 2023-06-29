6dfff20805c588ac22bab4e76400043e30c29f54
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-06-29 11:59:18 -0300

    feat: add static videos in VideoComponent

--- app/(home-components)/video-content.tsx
+++ app/(home-components)/video-content.tsx
@@ -1,29 +1,58 @@
+import Image from "next/image";
+
+
 export default function VideoContent() {
+   
+
   return (
-    <>
+    <main className="xl:w-[60%] mx-auto">
       <Filters></Filters>
-    </>
+      <VideosContainer></VideosContainer>
+    </main>
   );
+}
+
+function VideosContainer() {
+    const maxVideosOnDisplay = Array.from({ length: 9 }, (_, index) => index);
+
+    return (
+        <div className="flex justify-center w-full">
+            <div className="grid lg:grid-cols-3 gap-10">
+            {maxVideosOnDisplay.map((i) => <VideoContainer></VideoContainer>)}
+            </div>
+            
+        </div>
+    )
+
+}
+
+function VideoContainer() {
+    return (
+    <div className=" flex flex-col items-center justify-center bg-white rounded-xl drop-shadow-2xl shadow-black">
+        <Image height={250} width={1000} alt="Video Thumbnail" src={'/imgs/thumbnail.png'}></Image>
+        <p className="text-fontColor text-xs font-semibold  px-4 py-2">Como aumentar sua geração de Leads feat. Traktor</p>
+    </div>
+    )
 }
 
 function Filters() {
   return (
     <>
-      <div className="flex justify-center mt-16">
-        <div className="flex space-x-4 justify-center">
-          <p className="border-2 rounded-2xl border-fontColor py-2 px-4 text-fontColor font-semibold hover:border-main hover:text-main">
+      <div className="flex justify-between mt-16">
+        <div className="flex space-x-4">
+          <p className="border-2 rounded-2xl border-fontColor py-2 px-3  text-fontColor  font-semibold hover:border-main hover:text-white hover:bg-main">
             Agências
           </p>
-          <p className="border-2 rounded-2xl border-fontColor py-2 px-4 text-fontColor font-semibold hover:border-main hover:text-main">
+          <p className="border-2 rounded-2xl border-fontColor py-2 px-3 text-fontColor font-semibold hover:border-main hover:text-white hover:bg-main">
             Chatbot
           </p>
-          <p className="border-2 rounded-2xl border-fontColor py-2 px-4 text-fontColor font-semibold hover:border-main hover:text-main">
+          <p className="border-2 rounded-2xl border-fontColor py-2 px-3 text-fontColor font-semibold hover:border-main hover:text-white hover:bg-main">
             Marketing Digital
           </p>
-          <p className="border-2 rounded-2xl border-fontColor py-2 px-4 text-fontColor font-semibold hover:border-main hover:text-main">
+          <p className="border-2 rounded-2xl border-fontColor py-2 px-3 text-fontColor font-semibold hover:border-main hover:text-white hover:bg-main">
             Geração de Leads
           </p>
-          <p className="border-2 rounded-2xl border-fontColor py-2 px-4 text-fontColor font-semibold hover:border-main hover:text-main">
+          <p className="border-2 rounded-2xl border-fontColor py-2 px-3 text-fontColor font-semibold hover:border-main hover:text-white hover:bg-main">
             Mídia Paga
           </p>
         </div>
