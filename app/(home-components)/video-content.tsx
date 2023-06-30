a3c1598e21d120c214085e036fb5af659869b68e
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-06-29 21:13:23 -0300

    chore: dynamic display videoModal from JSON file

--- app/(home-components)/video-content.tsx
+++ app/(home-components)/video-content.tsx
@@ -2,11 +2,25 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import { useState } from "react";
+import videos from "@/public/assets/videos1.json"
 
 interface VideoModalProps {
+  video: VideoProps
   showModal?: boolean;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+interface VideoProps {
+  id: number;
+  urlId: string;
+  title: string;
+}
+
+interface VideoContainerProps {
+  video: VideoProps;
+}
+
+
 
 export default function VideoContent() {
   return (
@@ -18,20 +32,20 @@ export default function VideoContent() {
 }
 
 function VideosContainer() {
-  const maxVideosOnDisplay = Array.from({ length: 9 }, (_, index) => index);
+
 
   return (
     <div className="flex justify-center w-full">
       <div className="grid lg:grid-cols-3 gap-10">
-        {maxVideosOnDisplay.map((i) => (
-          <VideoContainer />
+        {videos.map((video) => (
+          <VideoContainer key={video.id} video={video}/>
         ))}
       </div>
     </div>
   );
 }
 
-function VideoContainer() {
+function VideoContainer(video: VideoContainerProps) {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
@@ -64,10 +78,58 @@ function VideoContainer() {
         </p>
        
       </div>
-      <VideoModal showModal={showModal} setShowModal={setShowModal} />
+      <VideoModal video={video.video} showModal={showModal} setShowModal={setShowModal} />
     </>
   );
 }
+
+
+
+function VideoModal({video, showModal, setShowModal }: VideoModalProps) {
+  return (
+    <div
+      onClick={() => setShowModal(false)}
+      className={` w-[100%] h-[100vh] bg-main bg-opacity-20 fixed bottom-0 left-0 flex items-center justify-center z-10 ${
+        showModal ? "block" : "hidden"
+      }`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white w-[450px] rounded-xl mx-auto my-auto relative"
+      >
+        <button
+          onClick={() => setShowModal(false)}
+          className="absolute top-3 right-3 w-[40px] h-[40px]"
+        >
+          X
+        </button>
+        <p className="text-fontColor font-semibold self-center mx-auto  p-5 w-[300px]">
+          <span className="text-main">Webinar: </span>  {video?.title}
+        </p>
+
+        {showModal && (
+          <iframe
+            width="450"
+            height="300"
+            src={`https://www.youtube.com/embed/${video?.urlId}`}
+          />
+        )}
+
+        <div className="py-2 px-5">
+          <p className="text-fontColor font-semibold">Descrição:</p>
+          <hr className="mx-auto my-6 h-[1px] w-full "></hr>
+          <p className="text-sm text-fontColor">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad
+            rem distinctio eum aliquam, tenetur magni incidunt explicabo!
+            Dolores earum fugiat culpa nisi praesentium dignissimos magni
+            suscipit blanditiis nihil nesciunt.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 function Filters() {
   return (
@@ -100,51 +162,5 @@ function Filters() {
 
       <hr className="mx-auto my-6 h-[1px] w-full"></hr>
     </>
-  );
-}
-
-function VideoModal({ showModal, setShowModal }: VideoModalProps) {
-  return (
-    <div
-      onClick={() => setShowModal(false)}
-      className={` w-[100%] h-[100vh] bg-main bg-opacity-20 fixed bottom-0 left-0 flex items-center justify-center z-10 ${
-        showModal ? "block" : "hidden"
-      }`}
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className="bg-white w-[450px] rounded-xl mx-auto my-auto relative"
-      >
-        <button
-          onClick={() => setShowModal(false)}
-          className="absolute top-3 right-3 w-[40px] h-[40px]"
-        >
-          X
-        </button>
-        <p className="text-fontColor font-semibold self-center mx-auto  p-5 w-[300px]">
-          <span className="text-main">Webinar:</span> Como aumentar sua Geração
-          de Leads feat. Traktor
-        </p>
-
-        {showModal && (
-          <iframe
-            width="450"
-            height="300"
-            src="https://www.youtube.com/embed/a89Htfx0YKo"
-          />
-        )}
-
-        <div className="py-2 px-5">
-          <p className="text-fontColor font-semibold">Descrição:</p>
-          <hr className="mx-auto my-6 h-[1px] w-full "></hr>
-          <p className="text-sm text-fontColor">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad
-            rem distinctio eum aliquam, tenetur magni incidunt explicabo!
-            Dolores earum fugiat culpa nisi praesentium dignissimos magni
-            suscipit blanditiis nihil nesciunt.
-          </p>
-        </div>
-      </div>
-    </div>
   );
 }
