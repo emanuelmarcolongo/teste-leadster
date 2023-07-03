e2053e218ed3c6418c8e1b817fb89f8458f9a86a
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-07-03 11:36:51 -0300

    chore: update responsive layout on filters

--- app/(home-components)/(video-content)/filter-component.tsx
+++ app/(home-components)/(video-content)/filter-component.tsx
@@ -19,8 +19,8 @@ export default function Filters({
   ];
   return (
     <>
-      <div className="flex justify-between mt-16">
-        <div className="flex space-x-4 overflow-x-scroll text-sm text-justify">
+      <div className="flex flex-col md:flex-row justify-between mt-16">
+        <div className="flex space-x-4 overflow-x-scroll text-sm text-justify mx-4">
           {filterOptions.map((option, idx) => 
         (<p
           key={idx}
@@ -29,16 +29,16 @@ export default function Filters({
             filter === option.key
               ? "text-white bg-main border-main"
               : "hover:text-main"
-          } hover:cursor-pointer  lg:border-2 py-1 px-1 lg:py-2 lg:px-3 rounded-xl border-fontColor   text-fontColor  font-semibold hover:border-main`}
+          }  hover:cursor-pointer border-2 py-1 px-1 lg:py-2 lg:px-3 rounded-xl border-fontColor   text-fontColor  font-semibold hover:border-main`}
         >
           {option.title}
         </p>)
           )}
 
         </div>
-        <div className="flex items-center justify-center ml-10">
+        <div className="flex flex-col max-w-[1/3] lg:flex-row items-center justify-center ml-10">
           <p className="text-fontColor font-semibold mr-2">Ordernar por</p>
-          <select className="bg-white border-2 rounded-2xl border-fontColor py-2 px-4">
+          <select className="bg-white border-2 rounded-2xl border-fontColor lg:py-2 lg:px-4 py-1 px-2">
             <option>Data de publicação</option>
           </select>
         </div>
